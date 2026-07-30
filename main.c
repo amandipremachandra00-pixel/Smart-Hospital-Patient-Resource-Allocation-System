1eff992ac4930ea8ac147f4b6953663fd6efb777
amandipremachandra00-pixel <amandipremachandra00@gmail.com>
2026-07-31 00:30:17 +0530

    debugging

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #define MAX 4
 #define SPECIALIST 4
-void printSpecialistData(char specialist[][4],float baseFee[],float consultantTime[],int patientCap[]);
+void printSpecialistData(char specialist[4][20],float baseFee[],float consultantTime[],int patientCap[]);
 
 int main()
 
-{char specialist[SPECIALIST][20]={"Genaral Practise(OPD)",
+{char specialist[4][20]={"Genaral Practise(OPD)",
                                    "Paediatrics",
                                    "cardiology",
                                    "Neurology"};
@@ -22,10 +22,10 @@ int patientCap[SPECIALIST]={30,
                             20,
                             12,
                             10};
- printSpecialistData(specialist[][SPECIALIST],baseFee,consultantTime, patientCap);
+ printSpecialistData(specialist,baseFee,consultantTime, patientCap);
 
 }
-void printSpecialistData(char specialist[][SPECIALIST],float baseFee[],float consultantTime[],int patientCap[]){
+void printSpecialistData(char specialist[4][20],float baseFee[],float consultantTime[],int patientCap[]){
 printf("%-20s%-20s%-20s%-20s%-20s","Specialty ID","Specialty Name","Base Consultation Fee(LKR)","Consaltation Time//Patient","Daily Patient Cap");
 for(int i=0;i<4;i++){
     scanf("%d%f%f%d",specialist[i][4],&baseFee[i],&consultantTime[i],&patientCap[i]);
