49d9ccfeba463082af507846530c6126b7697d70
amandipremachandra00-pixel <amandipremachandra00@gmail.com>
2026-08-03 23:28:59 +0530

    Updated reqirement 1

--- main.c
+++ main.c
@@ -2,14 +2,21 @@
 #include <stdlib.h>
 #define MAX 4
 #define SPECIALIST 4
-void printSpecialistData(char specialist[4][20],float baseFee[],float consultantTime[],int patientCap[]);
+#define WARDS 4
+void printSpecialistData(int ids[4][4],char specialist[4][20],float baseFee[],float consultantTime[],int patientCap[]);
+void printWardData(int wids[4][20],char wards[4][25],float bedRate[],int totalBed[]);
+void bedTracker( int bedOccupancy[4][20]);
 
 int main()
 
-{char specialist[4][20]={"Genaral Practise(OPD)",
-                                   "Paediatrics",
-                                   "cardiology",
-                                   "Neurology"};
+{int ids[4][4]={1,
+                2,
+                3,
+                4};
+char specialist[4][20]={"Genaral Practise OPD",
+                        "Paediatrics",
+                        "cardiology",
+                        "Neurology"};
 float baseFee[SPECIALIST]={1500.00,
                             2500.00,
                             4500.00,
@@ -22,13 +29,90 @@ int patientCap[SPECIALIST]={30,
                             20,
                             12,
                             10};
- printSpecialistData(specialist,baseFee,consultantTime, patientCap);
+
+int wids[4][20]={1,
+                2,
+                3,
+                4};
+char wards[4][25]={"Genaral Ward",
+                   "Paediatric Ward",
+                   "Surgical Ward",
+                   "ICU(Intensive Care Unit)"};
+float bedRate[WARDS]={3000.00,
+                      6000.00,
+                      12000.00,
+                      25000.00};
+int totalBed[WARDS]={20,
+                    10,
+                    10,
+                    05};
+int bedOccupancy[4][20];
+printSpecialistData(ids,specialist,baseFee,consultantTime, patientCap);
+printWardData(wids,wards,bedRate,totalBed);
+bedTracker(bedOccupancy);
+return 0;
+
+
+
+
 
 }
-void printSpecialistData(char specialist[4][20],float baseFee[],float consultantTime[],int patientCap[]){
-printf("%-20s%-20s%-20s%-20s%-20s","Specialty ID","Specialty Name","Base Consultation Fee(LKR)","Consaltation Time//Patient","Daily Patient Cap");
+void printSpecialistData(int ids[4][4],char specialist[4][20],float baseFee[],float consultantTime[],int patientCap[]){
+printf("Doctor Speecialist Data\n");
+printf("------------------------------------------------------------------------------------------------------------------------");
+printf("\n%-15s%-30s%-28s%-30s%-10s","Specialty ID","Specialty Name","Base Consultation Fee(LKR)","Consaltation Time/Patient","Daily Patient Cap");
+printf("\n-----------------------------------------------------------------------------------------------------------------------");
 for(int i=0;i<4;i++){
-    scanf("%d%f%f%d",specialist[i][4],&baseFee[i],&consultantTime[i],&patientCap[i]);
-    printf("%-20d%-20s%-20d%-20d%-20d",i+1,specialist[i][4],baseFee[i],consultantTime[i],patientCap[i]);
+
+    printf("\n%-15d%-35s%-30.2f%-30.2f%-10d\n",
+           i+1,
+           specialist[i],
+           baseFee[i],
+           consultantTime[i],
+           patientCap[i]);
+printf("\n-------------------------------------------------------------------------------------------------------------------------");
 }
 }
+void printWardData(int wids[4][20],char wards[4][25],float bedRate[],int totalBed[]){
+printf("\n");
+printf("\n");
+printf("\nHospital Wards Data");
+printf("\n---------------------------------------------------------------------------------------------------------------------");
+printf("\n%-15s%-35s%-25s%-15s","Ward Id","Ward Name","Daily Bed Rate(LKR/Day)","Total Bed Capacity" );
+printf("\n----------------------------------------------------------------------------------------------------------------------");
+for(int j=0;j<4;j++){
+    printf("\n%-15d%-35s%-25.2f%-15d\n",
+           j+1,
+           wards[j],
+           bedRate[j],
+           totalBed[j]);
+printf("\n-------------------------------------------------------------------------------------------------------------------");
+}
+}
+void bedTracker(int bedOccupancy[4][20]){
+for (int i=0;i<4;i++){
+
+for(int j=0;j<=20;j++){
+    bedOccupancy[i][j]=0;
+
+}
+}
+bedOccupancy[3][1]=1;
+bedOccupancy[2][2]=1;
+bedOccupancy[1][25]=1;
+bedOccupancy[2][5]=1;
+printf("--------------------Bed Status--------------------------------");
+for (int i=0;i<4;i++){
+    printf("\nWard number %d",i+1);
+
+for(int j=0;j<=20;j++){
+if(bedOccupancy[i][j]==1){
+    printf("\n%d= Bed Occupied",j);}
+else{
+    printf("\n%d= Bed aviable",j);
+}
+}
+}
+}
+
+
