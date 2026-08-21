26d99b99108da5c76dd56a74e337410f724066c1
amandipremachandra00-pixel <amandipremachandra00@gmail.com>
2026-08-21 19:51:24 +0530

    Updated requirement 2

--- main.c
+++ main.c
@@ -3,9 +3,11 @@
 #define MAX 4
 #define SPECIALIST 4
 #define WARDS 4
+#define MAX_PATIENTS 1000000
 void printSpecialistData(int ids[4][4],char specialist[4][20],float baseFee[],float consultantTime[],int patientCap[]);
 void printWardData(int wids[4][20],char wards[4][25],float bedRate[],int totalBed[]);
 void bedTracker( int bedOccupancy[4][20]);
+void patientregister(int i);
 
 int main()
 
@@ -46,10 +48,18 @@ int totalBed[WARDS]={20,
                     10,
                     10,
                     05};
+int i=0;
 int bedOccupancy[4][20];
+char name[MAX_PATIENTS];
+int age[MAX_PATIENTS];
+int patientlevel[MAX_PATIENTS];
+int specialid[MAX_PATIENTS];
+int ward[MAX_PATIENTS];
+int days[MAX_PATIENTS];
 printSpecialistData(ids,specialist,baseFee,consultantTime, patientCap);
 printWardData(wids,wards,bedRate,totalBed);
 bedTracker(bedOccupancy);
+patientregister(i);
 return 0;
 
 
@@ -113,6 +123,78 @@ else{
 }
 }
 }
+}
+void patientregister(int i){
+int choice;
+char name[MAX_PATIENTS];
+int age[MAX_PATIENTS];
+int patientlevel[MAX_PATIENTS];
+int specialid[MAX_PATIENTS];
+int ward[MAX_PATIENTS];
+int days[MAX_PATIENTS];
+printf("------------------------------------------------------------------------------------------------------------------");
+printf("\nPatient Details:");
+printf("\n-----------------------------------------------------------------------------------------------------------------");
+printf("\nEnter Patient Name ");
+scanf("%50s",name[i]);
+do{
+printf("\nEnter Patient Age");
+scanf("%d",&age[i]);
+if(age[i]<0){
+    printf("Invalid Age");
+}
+}
+while(age[i]<0);
+do{
+printf("\nEnter Emergency/Triage Level");
+printf("\n1=Normal\n2=Urgent\n3=Critical");
+scanf("%d",&patientlevel[i]);
+if(patientlevel[i]<1||patientlevel[i]>3){
+    printf("Invalid ");
+}
+}
+while(patientlevel[i]<1||patientlevel[i]>4);
+printf("\n-------------------------------------------------------------------------------------------------------------------");
+printf("\nSpecialty Selection");
+printf("\n----------------------------------------------------------------------------------------------------------------------");
+printf("\nEnter the Specialty ID");
+do{
+printf("\n1=Genaral Practice OPD\n2=Paediatrics\n3=Cardiology\n4=Neurology");
+scanf("%d",&specialid[i]);
+if(specialid[i]<1 || specialid[i]>4){
+    printf("Invalid Speciality ID");
+}
+}
+while(specialid[i]<1||specialid[i]>4);
+printf("\n-----------------------------------------------------------------------------------------------------------------------");
+printf("\nWard Admission Details");
+printf("\n------------------------------------------------------------------------------------------------------------------------");
+do{
+printf("\nIs Patient Admitt to Ward");
+printf("\n If Yes Enter 1\n If No Enter 0");
+scanf("%d",&choice);
+if (choice==1){
+   printf("Enter the Ward ID(1-4)");
+   printf("\n1=Genaral Ward\n2=Paediatric Ward\n3=Surgical Ward\n4=ICU(Intensive Care Unit)");
+   scanf("%d",&ward[i]);
+   printf("Enter the days Admitted");
+   scanf("%d",&days[i]);
+
+}
+else if(choice==0){
+    printf("Days Admitted=0 (Outpatient/OPD status)");
+}
+else{
+    printf("Invalid choice");
+}
+
+}
+while(choice!=0&&choice!=1);
+
+
+
+
+
 }
 
 
